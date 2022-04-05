1a13caf3736417475453dc9554137f52a4325235
Kyle Scott W <info@kylescottw.com>
2022-04-05 00:04:09 -0600

    feat: post comment http handler

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -12,7 +12,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type CommentService interface{}
+ 
 
 
 type Handler struct {
@@ -41,6 +41,10 @@ func (h *Handler) mapRoutes() {
 	h.Router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
+	h.Router.HandleFunc("/api/v1/comment", h.PostComment).Methods("POST")
+	h.Router.HandleFunc("/api/v1/comment/{id}", h.GetComment).Methods("GET")
+	h.Router.HandleFunc("/api/v1/comment/{id}", h.UpdateComment).Methods("PUT")
+	h.Router.HandleFunc("/api/v1/comment/{id}", h.DeleteComment).Methods("DELETE")
 }
 
 func (h *Handler) Serve() error {
